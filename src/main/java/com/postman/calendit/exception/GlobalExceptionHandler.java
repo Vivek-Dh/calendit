35e1ce37304eb9ec18b371492e72819c5fd4a575
VivekDh45 <vivekdh45@gmail.com>
2020-04-09 18:59:35 +0530

    Updating exception logging

--- src/main/java/com/postman/calendit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/postman/calendit/exception/GlobalExceptionHandler.java
@@ -14,16 +14,19 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(IllegalArgumentException.class)
   public ResponseEntity<String> handleIllegalArgumentException(Exception ex) {
+    logger.error(ex.getMessage(), ex);
     return new ResponseEntity<String>(ex.getMessage(), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(NullPointerException.class)
   public ResponseEntity<String> handleSQLException(HttpServletRequest request, Exception ex) {
+    logger.error(ex.getMessage(), ex);
     return new ResponseEntity<String>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<String> handleException(Exception ex) {
+    logger.error(ex.getMessage(), ex);
     return new ResponseEntity<String>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
